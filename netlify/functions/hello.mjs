2616ff128358501b54e1b9729b2168f4d9d8b878
Samuel Panicucci <samuelp88@gmail.com>
2024-09-07 12:22:40 +0200

    Andato avanti con la bozza di autenticazione JWT

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -1,11 +1,20 @@
 import { MongoClient } from "mongodb";
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
 
 export default async (request, context) => {
+    const tokenSecret = process.env.TOKEN_SECRET;
     const dbUrl = process.env.MONGO_CONNECTION;
+
+    const jwtCookieIn = context.cookies.get('jwt');
+    const payload = jwt.verify(jwtCookieIn, tokenSecret);
+
+    console.log(payload);
 
     const client = new MongoClient(dbUrl);
     let result = {};
+    let jwtCookie = '';
 
     const { name, email, password } = await request.json();
 
@@ -21,9 +30,18 @@ export default async (request, context) => {
                 name,
                 email,
                 password: hashedPassword
-            })
+            });
         }
 
+        const token = jwt.sign({email}, tokenSecret, {
+            expiresIn: '1 days'
+        });
+
+        jwtCookie = cookie.serialize('jwt', token, {
+            secure: process.env.NETLIFY_DEV !== "true",
+            httpOnly: true,
+            path: '/',
+        });
 
     } catch (err) {
 
@@ -31,5 +49,10 @@ export default async (request, context) => {
         client.close();
     }
 
-    return Response.json(result);
+    return Response.json(result, {
+        headers: {
+            'content-type': 'application/json',
+            'set-cookie': jwtCookie,
+        }
+    });
 }
